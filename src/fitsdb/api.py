c87e2bdee4636c2667832002bfc59b64c257cede
lgrcia <20612771+lgrcia@users.noreply.github.com>
2025-07-03 12:19:28 -0400

    fix: db path as environment variable + some docs

--- src/fitsdb/api.py
+++ src/fitsdb/api.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas as pd
 from fastapi import FastAPI
 
@@ -5,10 +7,7 @@ from fitsdb import db
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Welcome to": "fitsdb API"}
+db_path = os.environ.get("FITSDB", None)
 
 
 @app.get("/observations/")
@@ -18,7 +17,7 @@ def read_observations(
     date: str | None = None,
     object: str | None = None,
 ):
-    con = db.connect("dev/db.sql")
+    con = db.connect(db_path)
     obs = db.observations(
         con, instrument=instrument, filter=filter, date=date, object=object
     )
@@ -29,7 +28,7 @@ def read_observations(
 
 @app.get("/files/{index}")
 def read_files(index: int):
-    con = db.connect("dev/db.sql")
+    con = db.connect(db_path)
     files = pd.read_sql("SELECT * FROM files where id = ?", con, params=(index,))
     data = files.to_dict(orient="records")
     data = sorted(data, key=lambda x: x["date"])
